2428dc727e7b26977693fa8e8017bc2986461ae4
Andrew <github@awgm.co>
2026-01-08 14:09:26 +0000

    Update script path for michinomicon-dev app

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -2,7 +2,8 @@ module.exports = {
   apps: [
     {
       name: "michinomicon-dev",
-      script: "dist/server.js",
+      // script: "dist/server.js",
+      script: "node", args: ".next/standalone/server.js",
       // If using Payload 3.0 with Next.js, use: script: "npm", args: "start"
       env_production: {
         NODE_ENV: "production",
