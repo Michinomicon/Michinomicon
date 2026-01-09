cf66472d0a956180d5eacdf70e40b28a339853fd
that-andrew <github@awgm.co>
2026-01-09 13:59:15 +0800

    updated PM2 config

---
+++ ecosystem.config.cjs
@@ -0,0 +1,20 @@
+module.exports = {
+  apps: [
+    {
+      name: "michinomicon-dev",
+      script: "node", 
+      args: ".next/standalone/server.js",
+      // Force PM2 to read the .env file in the current directory
+      env_file: '.env',
+      env_production: {
+        NODE_ENV: "production",
+        PAYLOAD_CONFIG_PATH: "src/payload.config.ts",
+        // Email Settings
+        SMTP_HOST: "live.smtp.mailtrap.io",
+        SMTP_PORT: "587",
+        SMTP_USER: "api",
+        // SMTP_PASS: mailtrap API key will be loaded from .env
+      },
+    },
+  ],
+};
