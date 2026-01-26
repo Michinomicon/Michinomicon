08579a49d257b99e4852f038cd8159febc1fcab9
that-andrew <github@awgm.co>
2026-01-26 13:11:48 +0800

    alternate deploy github action with standalone build in pipeline

--- ecosystem.config.cjs
+++ ecosystem.config.cjs
@@ -1,31 +1,37 @@
 module.exports = {
   apps: [
     {
-      name: "michinomicon",
-      script: "pnpm", 
-      args: "start",
-      interpreter: "none", 
-      // Force PM2 to read the .env file in the current directory
+      name: 'michinomicon',
+      script: 'server.js', // Standalone entry point
+      //Force PM2 to read the .env file in the current directory
       env_file: '.env',
       env_production: {
-        NODE_ENV: "production",
-        PAYLOAD_CONFIG_PATH: "src/payload.config.ts",
+        NODE_ENV: 'production',
+        PAYLOAD_CONFIG_PATH: 'src/payload.config.ts',
       },
+      // Error handling and log settings
+      error_file: './logs/err.log',
+      out_file: './logs/out.log',
+      log_date_format: 'YYYY-MM-DD HH:mm:ss',
+      // Memory management: restart if it exceeds 400MB (adjust based on VPS specs)
+      max_memory_restart: '800M',
     },
   ],
-  // Deployment Configuration
-  // deploy : {
-  //   production : {
-  //     //"user" : process.env.SSH_USER,
-  //     //"host" : process.env.SSH_HOST,
-  //     //"ref"  : "origin/main",
-  //     //"repo" : "git@github.com:profile/repo.git",
-  //     //"path" : "/var/www/app-name",
-  //     //"pre-setup" : "echo 'commands or local script path to be run on the host before the setup process starts'",
-  //     //"post-setup": "echo 'commands or a script path to be run on the host after cloning the repo'",
-  //     //"pre-deploy" : "pm2 startOrRestart ecosystem.json --env production",
-  //     //"post-deploy" : "pm2 startOrRestart ecosystem.json --env production",
-  //     //"pre-deploy-local" : "echo 'This is a local executed command'"
-  //   }
-  // }
-};
+}
+
+// module.exports = {
+//   apps: [
+//     {
+//       name: "michinomicon",
+//       script: "pnpm",
+//       args: "start",
+//       interpreter: "none",
+//       // Force PM2 to read the .env file in the current directory
+//       env_file: '.env',
+//       env_production: {
+//         NODE_ENV: "production",
+//         PAYLOAD_CONFIG_PATH: "src/payload.config.ts",
+//       },
+//     },
+//   ],
+// };
