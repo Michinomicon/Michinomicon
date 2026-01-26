34cc50bc830588d673671a346a8a23f1261e4acb
that-andrew <github@awgm.co>
2026-01-26 17:44:12 +0800

    update deploy-standalone.yml

--- ecosystem.config.cjs
+++ ecosystem.config.cjs
@@ -2,12 +2,12 @@ module.exports = {
   apps: [
     {
       name: 'michinomicon',
-      script: 'server.js', // Standalone entry point
+      script: './server.js', // Standalone entry point
       //Force PM2 to read the .env file in the current directory
       env_file: '.env',
       env_production: {
         NODE_ENV: 'production',
-        PAYLOAD_CONFIG_PATH: 'src/payload.config.ts',
+        // PAYLOAD_CONFIG_PATH: 'src/payload.config.ts',
       },
       // Error handling and log settings
       error_file: './logs/err.log',
@@ -15,6 +15,7 @@ module.exports = {
       log_date_format: 'YYYY-MM-DD HH:mm:ss',
       // Memory management: restart if it exceeds 400MB (adjust based on VPS specs)
       max_memory_restart: '800M',
+      appendEnvToName: true,
     },
   ],
 }
