d06a502ed4164b28a445b3b189ff9d2a5c5d5c59
that-andrew <github@awgm.co>
2026-02-15 23:33:14 +0800

    fixed errors from rebase

--- src/collections/Categories.ts
+++ src/collections/Categories.ts
@@ -3,7 +3,6 @@ import type { CollectionConfig } from 'payload'
 import { anyone } from '../access/anyone'
 import { slugField } from 'payload'
 import { hasAccess } from '@/utilities/accessFunctions'
-import { hasAccess } from '@/utilities/accessFunctions'
 
 export const Categories: CollectionConfig = {
   slug: 'categories',
