1a27a192598cad1ef41b5afab6ddc9c5255ed5fc
that-andrew <github@awgm.co>
2026-06-14 11:26:21 +0800

    updated mobile navigation menu styles

--- src/payload.config.ts
+++ src/payload.config.ts
@@ -77,6 +77,7 @@ export default buildConfig({
   email: nodemailerAdapter({
     defaultFromAddress: 'system@michinomicon.com',
     defaultFromName: 'Michinomicon',
+    // Nodemailer transportOptions
     transportOptions: {
       host: process.env.SMTP_HOST,
       port: 587,
