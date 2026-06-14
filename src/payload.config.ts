99c86d13d15d01b0b23e281005e55761c8751fb0
that-andrew <github@awgm.co>
2026-06-14 11:43:06 +0800

    revert config change so CI passes.

--- src/payload.config.ts
+++ src/payload.config.ts
@@ -78,14 +78,14 @@ export default buildConfig({
     defaultFromAddress: 'system@michinomicon.com',
     defaultFromName: 'Michinomicon',
     // Nodemailer transportOptions
-    transportOptions: {
-      host: process.env.SMTP_HOST,
-      port: 587,
-      auth: {
-        user: process.env.SMTP_USER,
-        pass: process.env.SMTP_PASS,
-      },
-    },
+    // transportOptions: {
+    //   host: process.env.SMTP_HOST,
+    //   port: 587,
+    //   auth: {
+    //     user: process.env.SMTP_USER,
+    //     pass: process.env.SMTP_PASS,
+    //   },
+    // },
   }),
   // This config helps us configure global or default features that the other editors can inherit
   editor: defaultLexical,
