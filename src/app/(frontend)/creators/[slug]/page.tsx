be9808b3b4ae69a0499eac3a28b4b1ffb9eb3163
that-andrew <github@awgm.co>
2026-06-24 00:50:14 +0800

    improved layout and styling of image galleries

--- src/app/(frontend)/creators/[slug]/page.tsx
+++ src/app/(frontend)/creators/[slug]/page.tsx
@@ -5,7 +5,7 @@ import { PayloadRedirects } from '@/components/PayloadRedirects'
 import configPromise from '@payload-config'
 import { getPayload, PaginatedDocs } from 'payload'
 import { draftMode } from 'next/headers'
-import { cache } from 'react'
+import React, { cache } from 'react'
 import RichText from '@/components/RichText'
 import { generatePageOrPostMeta } from '@/utilities/generatePageOrPostMeta'
 import PageClient from './page.client'
@@ -47,6 +47,69 @@ type Args = {
   params: Promise<{
     slug?: string
   }>
+}
+
+type CreatorProfileSectionProps = { title: string; children?: React.ReactNode }
+const CreatorProfileSection = ({
+  title,
+  children,
+}: CreatorProfileSectionProps): React.ReactNode => {
+  return (
+    <div className="mb-6 flex w-full flex-col gap-4">
+      <Separator></Separator>
+      <div className="prose w-full">
+        <h2>{title}</h2>
+      </div>
+      {children}
+    </div>
+  )
+}
+
+type CreatorProfileLinkItemGroupProps = { links?: Creator['socialLinks'] }
+const CreatorProfileLinkItemGroup = ({
+  links,
+}: CreatorProfileLinkItemGroupProps): React.ReactNode => {
+  return (
+    <ItemGroup className="flex w-full flex-row flex-wrap gap-6">
+      {links &&
+        links.map(({ url, platform, id }) => (
+          <Item
+            key={id}
+            variant={'muted'}
+            className={'max-w-1/4 grow max-xl:max-w-1/3 max-md:min-w-full'}
+          >
+            <ItemContent>
+              <ItemTitle>{platform}</ItemTitle>
+              <ItemDescription>{url}</ItemDescription>
+            </ItemContent>
+            <ItemActions>
+              <CMSLink url={url} size="icon" appearance="ghost" className="rounded-full">
+                <ExternalLinkIcon className="size-4" />
+              </CMSLink>
+            </ItemActions>
+          </Item>
+        ))}
+    </ItemGroup>
+  )
+}
+
+type CreatorProfileHeaderProps = { creatorName: string; image: string | Media | null | undefined }
+const CreatorProfileHeader = ({
+  creatorName,
+  image,
+}: CreatorProfileHeaderProps): React.ReactNode => {
+  return (
+    <div className="mb-6 grid grid-cols-6 items-center gap-4">
+      <div className="col-span-2 col-start-1">
+        {image && typeof image === 'object' && <ImageMedia src={image} />}
+      </div>
+      <div className="col-span-4 col-start-3">
+        <span className="prose">
+          <h1 className="mb-6 text-3xl md:text-5xl lg:text-6xl">{creatorName}</h1>
+        </span>
+      </div>
+    </div>
+  )
 }
 
 export default async function Creator({ params: paramsPromise }: Args) {
