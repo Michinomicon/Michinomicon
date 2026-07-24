1bd350df8481f1724cc85f7cac2466437cb63a84
Andrew <github@awgm.co>
2026-07-24 12:12:32 +0800

    Bugfix image thumbnails not appearing on first load (#70)
    * bug fixes. additional logging to assist image src fixes
    
    * additional logging

--- src/utilities/getMediaUrl.ts
+++ src/utilities/getMediaUrl.ts
@@ -9,16 +9,22 @@ import { getClientSideURL } from '@/utilities/getURL'
 export const getMediaUrl = (url: string | null | undefined, cacheTag?: string | null): string => {
   if (!url) return ''
 
+  console.log(`getMediaUrl => IN: ${url} `)
+
   if (cacheTag && cacheTag !== '') {
     cacheTag = encodeURIComponent(cacheTag)
   }
 
   // Check if URL already has http/https protocol
   if (url.startsWith('http://') || url.startsWith('https://')) {
-    return cacheTag ? `${url}?${cacheTag}` : url
+    const alreadyPrepended = cacheTag ? `${url}?${cacheTag}` : url
+    console.log(`getMediaUrl => OUT(1): ${alreadyPrepended} `)
+    return alreadyPrepended
   }
 
   // Otherwise prepend client-side URL
   const baseUrl = getClientSideURL()
-  return cacheTag ? `${baseUrl}${url}?${cacheTag}` : `${baseUrl}${url}`
+  const formattedUrl = cacheTag ? `${baseUrl}${url}?${cacheTag}` : `${baseUrl}${url}`
+  console.log(`getMediaUrl => OUT(2): ${formattedUrl} `)
+  return formattedUrl
 }
