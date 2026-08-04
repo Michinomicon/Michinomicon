105f21386312d26118b3788c1b33c156d4dc0fc4
that-andrew <github@awgm.co>
2026-08-04 20:20:02 +0800

    updated playwright config

--- playwright.config.ts
+++ playwright.config.ts
@@ -27,7 +27,7 @@ export default defineConfig({
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
     /* Base URL to use in actions like `await page.goto('/')`. */
-    // baseURL: 'http://localhost:3000',
+    baseURL: 'http://localhost:3000',
 
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
     trace: 'on-first-retry',
@@ -39,9 +39,13 @@ export default defineConfig({
     },
   ],
   webServer: {
-    command: 'pnpm run build:clean && pnpm run start:standalone',
+    command: process.env.CI
+      ? 'pnpm run start:standalone'
+      : 'pnpm run build && pnpm run start:standalone',
     reuseExistingServer: !process.env.CI,
     url: 'http://127.0.0.1:3000',
     timeout: 300 * 1000, // 5 minutes
+    stdout: 'pipe',
+    stderr: 'pipe',
   },
 })
